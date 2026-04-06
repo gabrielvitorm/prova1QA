4446307c0bebb97da875b19fc4bdf5549f2c22b5
Gabriel Vitor <gabrielvitormchaves@gmail.com>
2026-04-06 19:39:07 -0300

    Initial commit

---
+++ src/main/java/br/com/prova1qa/api/ApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.prova1qa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/prova1qa/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.prova1qa.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
